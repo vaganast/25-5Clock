b7ca520f36848e7eff351db6549643eec615871f
vaganast <71605687+vaganast@users.noreply.github.com>
2023-01-10 17:31:57 +0200

    timer
    add sound function
    start count function update with interval for count

--- src/App.jsx
+++ src/App.jsx
@@ -53,31 +53,7 @@ const handleReset = () => {
     <div className="App">
       <div className='wrapper'>
         <h2> 25 + 5 Clock</h2>
-        <div className='container'>
-          <div>
-            <h3 id="break-label">Break Length</h3>
-            <div className='break-session-wrap'>
-              <button id="break-increment" onClick={handleCountPlusBreak}><FaChevronUp /></button>
-              <p id="break-length">{breakCount}</p>
-              <button id="break-decrement" onClick={handleCountMinusBreak}><FaChevronDown /></button>
-            </div>
-          </div>
-          <div>
-            <h3 id="session-label">Session Length</h3>
-            <div className='break-session-wrap'>
-              <button id="session-increment" onClick={handleCountPlusSession}><FaChevronUp /></button>
-              <p id="session-length">{sessionCount}</p>
-              <button id="session-decrement" onClick={handleCountMinusSession}><FaChevronDown /></button>
-            </div>
-          </div>
-        </div>
-        <div className='timer'>
-          <div className='timer-wrap'>
-            <Timer sTime={sessionCount} bTime={breakCount}/>
-          </div>
-        </div>
-            <button id="reset" style={{marginRight:'5px'}} onClick={handleReset}><FaUndo/></button>
-            <button id="start_stop"><FaPlay/><span></span><FaPause /></button>
+        <Timer />
       </div>
     </div>
   )
--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,18 +1,26 @@
 import React, { useState, useRef, useEffect } from 'react'
+import { FaPause, FaChevronUp, FaChevronDown, FaUndo, FaPlay } from "react-icons/fa";
+import alarm from './assets/alarm.mp3'
 
-const Timer = ({ sTime, bTime }) => {
-  //sTime=25m bTime=5m
-  let sessionMinutes = sTime * 60
-  let breakMinutes = bTime * 60
+const Timer = () => {
+  // set timers
+  const [breakCount, setBreakCount] = useState(5)
+  const [sessionCount, setSessionCount] = useState(0.1) //25 0.1 for testing
+  const [timer, setTimer] = useState(1500); //displaying time
+  const [timerOn, setTimerOn] = useState(false) //help to disable button if timer is on
+  const [playSound, setPlaySound] = useState(new Audio(alarm)) 
+  const [play, setPlay] = useState(true) // play pause state
 
-	// The state for our timer
-	const [timer, setTimer] = useState();
-  const [mode, setMode] = useState('work'); // work/break/null
+  //if we are on break timer
+  const [onBreak, setOnBreak] = useState(false)
 
-  const modeRef = useRef(mode);
   const timerId = useRef();
-
-  // formation for time via props
+  
+  const playAlarm = () => {
+    playSound.currentTime = 1
+    playSound.play()
+  }
+  // formation for time displaying mm:ss
   const formatTime = (time) => {
     let minutes = Math.floor(time/60)
     let seconds = time % 60   
@@ -23,35 +31,120 @@ const Timer = ({ sTime, bTime }) => {
 
   //update the display time
   useEffect(() => {  
-   setTimer(sessionMinutes)
-  }, [sessionMinutes])	
+   if(!timerOn){
+    setTimer(sessionCount*60)
+   }
+  }, [sessionCount])	  
 
-  //start / stop / reset functions
-  const startCount = () => {
-    timerId.current = setInterval(() => {
-      setTimer((prev) => prev - 1)
-    }, 1000)
+  //start / pause functions
+  const startCount = () => {      
+    let onBreakVariable = onBreak
+    console.log({onBreakVariable});
+    if(play) {
+     timerId.current = setInterval(() => {
+        setTimer((prev) => {
+          if (prev <=0 && !onBreakVariable) {  
+            setOnBreak(true)
+            playAlarm()          
+            onBreakVariable=true
+            console.log({onBreak});
+            return breakCount*60;
+          } else if (prev <=0 && onBreakVariable) { 
+            playAlarm()           
+            onBreakVariable=false
+            setOnBreak(false)
+            return sessionCount*60;
+          } else
+          return prev - 1})
+      }, 1000)      
+      setPlay(!play)  
+      setTimerOn(!timerOn)   
+    }     
+    if(!play) {
+      clearInterval(timerId.current)
+      timerId.current = 0
+      setPlay(!play)
+      setTimerOn(!timerOn) 
+    }    
   }
+      // into the else statement above for the pause
+  // const stopCount = () => {
+  //   clearInterval(timerId.current)
+  //   timerId.current = 0
+  // }
 
-  const stopCount = () => {
-    clearInterval(timerId.current)
-    timerId.current = 0
-  }
-
+  //reset function
   const resetCount = () => {
-    stopCount()
+    clearInterval(timerId.current)
+     timerId.current = 0  
+     setBreakCount(5)
+  setSessionCount(25) 
+  setTimerOn(false)
     if (timer) {
-      setTimer(sessionMinutes)
+      setTimer(1500) //25 * 60
     }
   }
 
+  //handlers + - session break
+const handleCountPlusBreak = () => { 
+  if (breakCount == 60 ) {
+    return 
+  } else {
+    setBreakCount((prev) => prev + 1)
+  } 
+}
+
+const handleCountMinusBreak = () => {
+  if (breakCount == 1) {
+    return
+  } else {
+    setBreakCount((prev) => prev - 1)
+  }
+}
+
+const handleCountPlusSession = () => {
+  if (sessionCount == 60) {
+    return
+  } else {
+    setSessionCount((prev) => prev + 1)
+  } 
+}
+
+const handleCountMinusSession = () => {
+  if (sessionCount == 1) {
+    return
+  } else {
+    setSessionCount((prev) => prev - 1)
+  }
+}
 	return (
 		<>
-     <p id="timer-label">Session / break </p>
-     <p id="time-left" style={{fontSize: '40px'}}>{formatTime(timer)}</p>
-     <button onClick={startCount}>test count button</button>
-     <button onClick={stopCount}>test stop count button</button>
-     <button onClick={resetCount}>test reset count button</button>
+    <div className='container'>
+          <div>
+            <h3 id="break-label">Break Length</h3>
+            <div className='break-session-wrap'>
+              <button id="break-increment" disabled={timerOn} onClick={handleCountPlusBreak}><FaChevronUp/></button>
+              <p id="break-length">{breakCount}</p>
+              <button id="break-decrement" disabled={timerOn} onClick={handleCountMinusBreak}><FaChevronDown /></button>
+            </div>
+          </div>
+          <div>
+            <h3 id="session-label">Session Length</h3>
+            <div className='break-session-wrap'>
+              <button id="session-increment" disabled={timerOn} onClick={handleCountPlusSession}><FaChevronUp /></button>
+              <p id="session-length">{sessionCount}</p>
+              <button id="session-decrement" disabled={timerOn} onClick={handleCountMinusSession}><FaChevronDown /></button>
+            </div>
+          </div>
+        </div>
+        <div className='timer'>
+          <div className='timer-wrap'>
+          <p id="timer-label"> {onBreak ? 'Break' : 'Session'}</p>
+     <p id="time-left" style={{fontSize: '40px'}}>{formatTime(timer)}</p>   
+          </div>
+        </div>
+            <button id="reset" style={{marginRight:'5px'}} onClick={resetCount}><FaUndo/></button>
+            <button id="start_stop" onClick={startCount}>{play ? <FaPlay /> : <FaPause />}</button>
     </>
 	)
 }
