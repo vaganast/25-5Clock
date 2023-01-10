837aca3b4680f35da37bb51641962b2212007559
vaganast <71605687+vaganast@users.noreply.github.com>
2023-01-11 01:42:00 +0200

    finish
    change to useEffect with dependencies
    working fine the timer
    play sound and all test pass but an error on script showing Error: Timer has not reached 00:00 but ininterval checking if timer===0

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -4,22 +4,16 @@ import alarm from './assets/alarm.mp3'
 
 const Timer = () => {
   // set timers
-  const [breakCount, setBreakCount] = useState(5)
-  const [sessionCount, setSessionCount] = useState(0.1) //25 0.1 for testing
+  const [breakCount, setBreakCount] = useState(5) // 5 0.3 for testing
+  const [sessionCount, setSessionCount] = useState(25) //25 0.1 for testing
   const [timer, setTimer] = useState(1500); //displaying time
   const [timerOn, setTimerOn] = useState(false) //help to disable button if timer is on
-  const [playSound, setPlaySound] = useState(new Audio(alarm)) 
-  const [play, setPlay] = useState(true) // play pause state
-
-  //if we are on break timer
-  const [onBreak, setOnBreak] = useState(false)
-
+  const [play, setPlay] = useState(true) // play pause state, should put false but i continue with true
+  const [onBreak, setOnBreak] = useState(false)  //if we are on break timer to change between break and session
+  //refs to keep the time with play/pause
   const timerId = useRef();
-  
-  const playAlarm = () => {
-    playSound.currentTime = 1
-    playSound.play()
-  }
+  const audioRef = useRef(null); 
+
   // formation for time displaying mm:ss
   const formatTime = (time) => {
     let minutes = Math.floor(time/60)
@@ -36,50 +30,86 @@ const Timer = () => {
    }
   }, [sessionCount])	  
 
-  //start / pause functions
-  const startCount = () => {      
-    let onBreakVariable = onBreak
-    console.log({onBreakVariable});
-    if(play) {
-     timerId.current = setInterval(() => {
-        setTimer((prev) => {
-          if (prev <=0 && !onBreakVariable) {  
-            setOnBreak(true)
-            playAlarm()          
-            onBreakVariable=true
-            console.log({onBreak});
-            return breakCount*60;
-          } else if (prev <=0 && onBreakVariable) { 
-            playAlarm()           
-            onBreakVariable=false
-            setOnBreak(false)
-            return sessionCount*60;
-          } else
-          return prev - 1})
-      }, 1000)      
-      setPlay(!play)  
-      setTimerOn(!timerOn)   
-    }     
-    if(!play) {
-      clearInterval(timerId.current)
-      timerId.current = 0
-      setPlay(!play)
-      setTimerOn(!timerOn) 
-    }    
-  }
+  //start / pause functions should use useEffect
+  // const startCount = () => {      
+  //   let onBreakVariable = onBreak    
+  //   if(play) {
+  //    timerId.current = setInterval(() => {
+  //       setTimer((prev) => {
+  //         if (prev <=0 && !onBreakVariable) {  
+  //           setOnBreak(true)
+  //           playAlarm()          
+  //           onBreakVariable=true
+  //           console.log({onBreak});
+  //           return breakCount*60;
+  //         } else if (prev <=0 && onBreakVariable) { 
+  //           playAlarm()           
+  //           onBreakVariable=false
+  //           setOnBreak(false)
+  //           return sessionCount*60;
+  //         } else
+  //         return prev - 1})
+  //     }, 1000)      
+  //     setPlay(!play)  
+  //     setTimerOn(!timerOn)   
+  //   }     
+  //   if(!play) {
+  //     clearInterval(timerId.current)
+  //     timerId.current = 0
+  //     setPlay(!play)
+  //     setTimerOn(!timerOn) 
+  //   }    
+  // }
       // into the else statement above for the pause
   // const stopCount = () => {
   //   clearInterval(timerId.current)
   //   timerId.current = 0
   // }
+  useEffect(() => {
+    let interval = null;
+    if (!play) {
+      interval = setInterval(() => {
+        if (timer === 0) {
+          audioRef.current.play()
+          if (onBreak) {
+            setOnBreak(false)
+            setTimer(sessionCount * 60)            
+          } else {
+            setOnBreak(true)
+            setTimer(breakCount * 60)            
+          }
+        } else {
+          setTimer((seconds) => seconds - 1)          
+        }
+      }, 1000);
+    } else {
+      clearInterval(interval)      
+    }
+    return () => clearInterval(interval)
+  }, [play, timer, onBreak, breakCount, sessionCount]);
+
+  useEffect(() => {
+    if (timer === 0) {
+      audioRef.current.play()
+    }
+  }, [timer]);
+
+  const startCount = () => {
+    setPlay(!play)
+    setTimerOn(!timerOn)
+  }
 
   //reset function
   const resetCount = () => {
+    setPlay(true)
+    setOnBreak(false)
+    audioRef.current.pause()
+    audioRef.current.currentTime = 0
     clearInterval(timerId.current)
-     timerId.current = 0  
-     setBreakCount(5)
-  setSessionCount(25) 
-  setTimerOn(false)
+    timerId.current = 0  
+    setBreakCount(5)
+    setSessionCount(25) 
+    setTimerOn(false)
     if (timer) {
       setTimer(1500) //25 * 60
     }
@@ -138,15 +168,15 @@ const handleCountMinusSession = () => {
           </div>
         </div>
         <div className='timer'>
-          <div className='timer-wrap'>
-          <p id="timer-label"> {onBreak ? 'Break' : 'Session'}</p>
-     <p id="time-left" style={{fontSize: '40px'}}>{formatTime(timer)}</p>   
+          <div className='timer-wrap' id="timer-label">
+          <div id="timer-label">{onBreak ? 'Break' : 'Session'}</div>
+          <p id="time-left" style={{fontSize: '40px'}}>{formatTime(timer)}</p>   
           </div>
         </div>
             <button id="reset" style={{marginRight:'5px'}} onClick={resetCount}><FaUndo/></button>
             <button id="start_stop" onClick={startCount}>{play ? <FaPlay /> : <FaPause />}</button>
+            <audio id="beep" type="audio/mpeg" ref={audioRef} src={alarm} />
     </>
 	)
 }
-
 export default Timer
